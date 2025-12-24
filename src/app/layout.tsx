87463b1a659c9e38fa59c713acdb56a5040e7ac9
JoeChen <bibirock0104@gmail.com>
2025-12-24 18:29:46 +0800

    [adjust]: 調整 移除預設的全域樣式

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,13 @@
+/*
+ * @Author: JoeChen
+ * @Date: 2025-12-24 17:48:21
+ * @LastEditors: JoeChen bibirock0104@gmail.com
+ * @LastEditTime: 2025-12-24 18:26:59
+ * @Description:
+ */
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import StyledComponentsRegistry from "@/lib/registry";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +21,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
     </html>
   );
