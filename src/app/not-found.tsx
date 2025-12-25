82e64d33bd48317bc95b4b3b8c8e52b855792108
JoeChen <bibirock0104@gmail.com>
2025-12-25 12:21:11 +0800

    [adjust]: 調整 將預設路由導向 /heroes 頁面，及處理 404 頁面重新導向

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,12 @@
 /*
  * @Author: JoeChen
- * @Date: 2025-12-23 15:25:53
+ * @Date: 2025-12-25
  * @LastEditors: JoeChen bibirock0104@gmail.com
- * @LastEditTime: 2025-12-25 12:18:33
- * @Description: Redirect root to /heroes
+ * @LastEditTime: 2025-12-25
+ * @Description: 404 page - redirects to /heroes
  */
 import { redirect } from "next/navigation";
 
-export default function Home() {
+export default function NotFound() {
   redirect("/heroes");
 }
